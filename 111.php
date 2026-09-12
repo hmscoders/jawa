3df70de5597aa14750b6a715acbca45d7da91a9e
hmscoders <nocharmonikaidpc@gmail.com>
2026-09-12 11:44:11 +0700

    Update 111.php

--- 111.php
+++ 111.php
@@ -275,7 +275,7 @@ $self = str_replace('//', '/', $self);
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Rehan1337 | 1WP User Helper</title>
+    <title>Rehan1337 | WP User Helper</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet">
     <style>
